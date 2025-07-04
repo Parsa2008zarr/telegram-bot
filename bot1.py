791466dd6e4b7fa549a83c6c0d21185779eda7dd
PARSA <Parsa2008z@gmail.com>
2025-07-04 22:31:25 +0330

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-# دستور شروع
+ شروع
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f"سلام {update.effective_user.first_name}! من یه ربات ساده‌ام.")
 
